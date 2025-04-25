67797ea9638e140ed020d321487e5466f64902ca
Le Thanh Vinh <thanhvinh2400@gmail.com>
2025-04-25 11:13:35 +0700

    Create function to get user,object's system, grant privilege

--- OUM/OUM/Service/DataAccess/OracleDAO.cs
+++ OUM/OUM/Service/DataAccess/OracleDAO.cs
@@ -1,4 +1,5 @@
-﻿using Oracle.ManagedDataAccess.Client;
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using Oracle.ManagedDataAccess.Client;
 using OUM.Model;
 using OUM.Session;
 using System;
@@ -26,7 +27,7 @@ namespace OUM.Service.DataAccess
             }
             catch
             {
-                return false;
+                return true;
             }
         }
         public string GetConnectionString()
@@ -60,8 +61,8 @@ namespace OUM.Service.DataAccess
         public List<Employee> GetListEmployees()
         {
             List<Employee> employees = new List<Employee>();
-
-            using (var connection = new OracleConnection(GetConnectionString()))
+            string connect = GetConnectionString();
+            using (var connection = new OracleConnection(connect))
             {
                 try
                 {
@@ -96,6 +97,7 @@ namespace OUM.Service.DataAccess
                 {
                     Console.WriteLine("Lỗi khi lấy danh sách nhân viên: " + ex.Message);
                 }
+
             }
 
             return employees;
@@ -145,5 +147,153 @@ namespace OUM.Service.DataAccess
             return students;
         }
 
+
+        public List<UserSystemInformation> GetSystemUsers(string keyword)
+        {
+            List<UserSystemInformation> users = new List<UserSystemInformation>();
+            try
+            {
+                using (var connection = new OracleConnection(GetConnectionString()))
+                {
+                    connection.Open();
+                    string query = @"SELECT username, account_status,created FROM dba_users
+                                    WHERE oracle_maintained ='N'";
+                    var command = new OracleCommand(query, connection);
+                    var reader =command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        UserSystemInformation user = new UserSystemInformation
+                        (
+                            username: reader["USERNAME"].ToString(),
+                            status: reader["ACCOUNT_STATUS"].ToString(),
+                            created: Convert.ToDateTime(reader["CREATED"])
+                        );
+                        users.Add(user);
+                    }
+                    
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error when get system user: " + ex.Message);
+            }
+            return users;
+        }
+
+        public List<DatabaseObject> GetDatabaseObject(string objectType)
+        {
+            List<DatabaseObject> DBobjects = new List<DatabaseObject>();
+            string query ;
+            if(string.IsNullOrEmpty(objectType)||objectType == "ALL")
+            {
+                query = @"SELECT OBJECT_NAME, OBJECT_TYPE,STATUS,CREATED FROM ALL_OBJECTS
+                          WHERE OWNER =:owner and OBJECT_TYPE in ('TABLE','VIEW','FUNCTION','PROCEDURE')";
+            }
+            else
+            {
+                query = @"SELECT OBJECT_NAME, OBJECT_TYPE,STATUS,CREATED FROM ALL_OBJECTS
+                          WHERE OWNER =:owner and OBJECT_TYPE = :objectType";
+            }
+            try
+            {
+                using (var connection = new OracleConnection(GetConnectionString()))
+                {
+                    connection.Open();
+                    var command = new OracleCommand(query, connection);
+                    command.Parameters.Add(new OracleParameter("owner", AdminSession.Username.ToUpper()));
+                    if(!string.IsNullOrEmpty(objectType)&&objectType!="ALL")
+                    {
+                        command.Parameters.Add(new OracleParameter("objectType", objectType));
+
+                    }
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        DatabaseObject dbobject = new DatabaseObject
+                        (
+                            name: reader["OBJECT_NAME"].ToString(),
+                            type: reader["OBJECT_TYPE"].ToString(),
+                            created: Convert.ToDateTime(reader["CREATED"]),
+                            status: reader["STATUS"].ToString()
+                        );
+                        DBobjects.Add(dbobject);
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error when get system user: " + ex.Message);
+            }
+            return DBobjects;
+        }
+        public List<string> GetColumsForObject(string objectName,string objectType)
+        {
+            List<string> columns = new List<string>();
+            string query;
+            if (objectType=="TABLE"||objectType=="VIEW")
+            {
+                query = @"SELECT COLUMN_NAME FROM ALL_TAB_COLUMNS
+                          WHERE OWNER =:owner and TABLE_NAME= :objecName";
+                try
+                {
+                    using (var connection = new OracleConnection(GetConnectionString()))
+                    {
+                        connection.Open();
+                        var command = new OracleCommand(query, connection);
+                        command.Parameters.Add(new OracleParameter("owner", AdminSession.Username.ToUpper()));
+                        command.Parameters.Add(new OracleParameter("objecName", objectName.ToUpper()));
+                        var reader = command.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            columns.Add(reader.GetString(0));
+                        }
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error when get system user: " + ex.Message);
+                }
+            }
+            return columns;
+        }
+        public bool GrantPrivilege(string username, string objectName, string privilege, List<string> columns =null, bool withGrantOptin = false)
+        {
+            try
+            {
+                using (var connection =new OracleConnection(GetConnectionString()))
+                {
+                    connection.Open();
+                    string sql;
+                    string grantOptionClause = withGrantOptin ? "WITH GRANT OPTION" : "";
+                    if ( privilege=="UPDATE" && columns!=null && columns.Count > 0)
+                    {
+                        if (columns.Contains("ALL"))
+                        {
+                            sql = $"GRANT {privilege} ON {objectName} TO {username} {grantOptionClause}";
+                        }
+                        else
+                        {
+                            sql = $"GRANT {privilege}({string.Join(", ",columns)}) ON {objectName} TO {username} {grantOptionClause}";
+                        }
+                    }
+                    else
+                    { 
+                        sql = $"GRANT {privilege} ON {objectName} TO {username} {grantOptionClause}";
+                    }
+                    using (var command = new OracleCommand(sql, connection))
+                    {
+                        command.ExecuteNonQuery();
+                        return true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error when grant privilege: " + ex.Message);
+                return false;
+            }
+        }
     }
 }

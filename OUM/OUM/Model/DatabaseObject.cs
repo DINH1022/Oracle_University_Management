2eb763286e0ee90e075008f1e4d64219d2c05d0a
Le Thanh Vinh <thanhvinh2400@gmail.com>
2025-04-25 11:14:39 +0700

    Create DatabaseObject Model

---
+++ OUM/OUM/Model/DatabaseObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OUM.Model
+{
+    public class DatabaseObject :INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+        public string ObjectType { get; set; }
+        public DateTime Created { get; set; }
+        public string Status { get; set; }
+        public DatabaseObject(string name,string type,DateTime created, string status) { 
+            this.Name = name;
+            this.ObjectType = type;
+            this.Created = created;
+            this.Status = status;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}

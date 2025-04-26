2a7d895d49deecb14ebb60e0def0424361c00bab
22120433 <lequangvinh.127.2022@gmail.com>
2025-04-26 21:20:32 +0700

    add some file

---
+++ OUM/OUM/Model/UserInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OUM.Model
+{
+    public class UserInfo
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserType { get; set; }
+
+        public UserInfo(string userId, string userName, string userType)
+        {
+            this.UserId = userId;
+            this.UserName = userName;
+            this.UserType = userType;
+        }
+
+        public override string ToString()
+        {
+            return UserId;
+        }
+    }
+}
